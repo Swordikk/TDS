2b7c05fae0c5a240d740cb132c5e013d9f6b3070
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-10-01 16:20:05 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,11 +5,6 @@ local Window = OrionLib:MakeWindow({Name = "Script by Swordik | ⚡Tower Defense
 local Humanoid = game.Players.LocalPlayer.Character.Humanoid
 local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
 
-local Skip = {
-    [1] = "Voting",
-    [2] = "Skip"
-}
-
 local Commander = {
     [1] = "Troops",
     [2] = "Abilities",
@@ -21,29 +16,10 @@ local Commander = {
     }
 }
 
-function AutoSkip()
-    while _G.AutoSkip == true do
-        game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(Skip))
-        wait(0.1)
-    end
-end
-
---[[function AutoCommander()
+function AutoCommander()
     while _G.AutoCommander == true do
         game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(Commander))
         wait(10.5)
-    end
-end]]--
-
-function WalkSpeed()
-	while _G.WalkSpeed do game:GetService("RunService").RenderStepped:wait()
-	    Humanoid.WalkSpeed = _G.WalkSpeed
-    end
-end
-
-function JumpPower()
-	while _G.JumpPower do game:GetService("RunService").RenderStepped:wait()
-	    Humanoid.JumpPower = _G.JumpPower
     end
 end
 
@@ -54,65 +30,10 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddToggle({
-	Name = "Auto Skip",
-	Default = false,
-	Callback = function(Value)
-		_G.AutoSkip = Value
-		AutoSkip()
-	end    
-})
-
-Tab:AddToggle({
 	Name = "AutoCommander (turn on when 3 commanders)",
 	Default = false,
 	Callback = function(Value)
-		if Value == true then
-            game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(Commander))
-        end
+		_G.AutoCommander = Value
+		AutoCommander()
 	end    
-})
-
-Tab:AddTextbox({
-	Name = "WalkSpeed",
-	Default = "",
-	TextDisappear = false,
-	Callback = function(Value)
-		_G.WalkSpeed = Value
-		WalkSpeed()
-	end
-})
-
-Tab:AddTextbox({
-	Name = "JumpPower",
-	Default = "",
-	TextDisappear = false,
-	Callback = function(Value)
-		_G.JumpPower = Value
-		JumpPower()
-	end
-})
-
-Tab:AddToggle({
-	Name = "Anti-AFK",
-	Default = false,
-	Callback = function(Value)
-		if Value == true then
-			while not game:IsLoaded() do wait() end
-			repeat wait() until game.Players.LocalPlayer.Character
-			Players = game:GetService("Players")
-			local GC = getconnections or get_signal_cons
-			if GC then
-				for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
-					if v["Disable"] then v["Disable"](v)
-					elseif v["Disconnect"] then v["Disconnect"](v)
-					end
-				end
-			else
-			Players.LocalPlayer.Idled:Connect(function()
-				VirtualUser:CaptureController()
-				VirtualUser:ClickButton2(Vector2.new())
-  				end)
-			end
-		end
-	end
 })
